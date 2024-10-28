a410bc7dbc395d56466f711b168af86e5497795d
Daniel Stacoffe <daniel.stacoffe@wanadoo.fr>
2024-10-28 17:20:12 +0100

    Ajout carte des vins

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -6,31 +6,129 @@ import Vins from "./components/Vins";
 import Liste_Vins from "./components/Liste_Vins";
 
 const ListeRegion = [
-	{ region: "", imageRegion: "", id: 0 },
-	{ region: "", imageRegion: "", id: 1 },
-	{ region: "", imageRegion: "", id: 2 },
-	{ region: "", imageRegion: "", id: 3 },
+  { region: "", imageRegion: "", id: 0 },
+  { region: "", imageRegion: "", id: 1 },
+  { region: "", imageRegion: "", id: 2 },
+  { region: "", imageRegion: "", id: 3 },
 ];
 
 const ListeVins = [
-	[{ nom: "", category: "", description: "", appellation: "" }],
+  [
+    {
+      nom: "Clos de Vougeot",
+      category: "Vin Rouge Grand Cru",
+      description: "Bourgogne Grand Cru",
+      appellation: "Côte de Nuits",
+      imageVin: "",
+    },
+    {
+      nom: "Grands Echézeaux",
+      category: "Vin Rouge Grand Cru",
+      description: "Bourgogne Grand Cru",
+      appellation: "Côte de Nuits",
+      imageVin: "",
+    },
+    {
+      nom: "Romanée Conti",
+      category: "Vin Rouge Grand Cru",
+      description: "Bourgogne Grand Cru",
+      appellation: "Côte de Nuits",
+      imageVin: "",
+    },
+    {
+      nom: "Corton Charlemagne",
+      category: "Vin Rouge Grand Cru",
+      description: "Bourgogne Grand Cru",
+      appellation: "Côte de Nuits",
+      imageVin: "",
+    },
+  ],
+  [
+    {
+      nom: "Château LAFITE-ROTHSCHILD",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+  ],
+  [
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+  ],
+  [
+    {
+      nom: "",
+      category: "Vin Rouge",
+      description: "",
+      appellation: "Bourgogne",
+      imageVin: "",
+    },
+  ],
 ];
 
 function App() {
-	const [index, setIndex] = useState(null);
+  const [index, setIndex] = useState(null);
 
-	return (
-		<>
-			<nav>
-				<Navbar />
-				<Liste_Vins />
-			</nav>
-			<div>
-				<Regions />
-				<Vins />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <nav>
+        <Navbar />
+        <Liste_Vins />
+      </nav>
+      <div>
+        <Regions />
+        <Vins Liste={ListeVins[0]} />
+      </div>
+    </>
+  );
 }
 
 export default App;
